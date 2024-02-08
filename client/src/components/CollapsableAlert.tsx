38f4f8336eb57b044602a7526722ca074309495f
Shristi Sharan <shristisharan05@gmail.com>
2024-02-08 23:56:06 +0530

    Error solved

--- client/src/components/CollapsableAlert.tsx
+++ client/src/components/CollapsableAlert.tsx
@@ -1,33 +1,33 @@
-import { Close } from '@mui/icons-material';
-import { Alert, Collapse, IconButton, SxProps, Theme } from '@mui/material';
-import { useContext } from 'react';
-import { Context } from '../data/Context';
+// import { Close } from '@mui/icons-material';
+// import { Alert, Collapse, IconButton, SxProps, Theme } from '@mui/material';
+// import { useContext } from 'react';
+// import { Context } from '../data/Context';
 
-export default function CollapsableAlert(props: { sx?: SxProps<Theme> }) {
-  const { sx } = props;
-  const { error, setError } = useContext(Context);
+// export default function CollapsableAlert(props: { sx?: SxProps<Theme> }) {
+//   const { sx } = props;
+//   const { error, setError } = useContext(Context);
 
-  return (
-    <Collapse in={error.open}>
-      <Alert
-        action={
-          <IconButton
-            aria-label='close'
-            color='inherit'
-            size='small'
-            onClick={() => {
-              setError({ ...error, open: false });
-            }}
-          >
-            <Close fontSize='inherit' />
-          </IconButton>
-        }
-        severity={error.severity}
-        variant='filled'
-        sx={sx}
-      >
-        {error.message}
-      </Alert>
-    </Collapse>
-  );
-}
+//   return (
+//     <Collapse in={error.open}>
+//       <Alert
+//         action={
+//           <IconButton
+//             aria-label='close'
+//             color='inherit'
+//             size='small'
+//             onClick={() => {
+//               setError({ ...error, open: false });
+//             }}
+//           >
+//             <Close fontSize='inherit' />
+//           </IconButton>
+//         }
+//         severity={error.severity}
+//         variant='filled'
+//         sx={sx}
+//       >
+//         {error.message}
+//       </Alert>
+//     </Collapse>
+//   );
+// }
